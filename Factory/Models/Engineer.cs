bbeb56b6129743bbfb8c04fe0f719fb267416b00
Ben Wilson <benjaminw1030@gmail.com>
2021-10-15 08:38:46 -0700

    add Engineer and EngineerMachine models

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+    public int EngineerId { get; set; }
+    public string Name { get; set; }
+    [Display(Name = "Job Title")]
+    public string JobTitle { get; set; }
+    [Display(Name = "Hire Date")]
+    public DateTime HireDate { get; set; }
+    public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+  }
+}
